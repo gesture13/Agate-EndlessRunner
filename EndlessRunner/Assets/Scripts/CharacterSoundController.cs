007bc838581ac7d050cea5af1a2e879f23bf8b66
gesture13 <aputraherdani@gmail.com>
2021-09-15 22:45:29 +0700

    First Commit

---
+++ EndlessRunner/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+    public AudioClip gameOverSound;
+
+    private AudioSource audioPlayer;
+
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    public void PlayGameOverSound()
+    {
+        audioPlayer.PlayOneShot(gameOverSound);
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+}
---
+++ EndlessRunner/README.md
@@ -0,0 +1,12 @@
+# Endless Runner
+Endless Runner, is a game where the player will move to the right continuously through obstacles without any restrictions. The further the player moves, the greater the score obtained.
+
+## Features
+1. Background music by https://www.youtube.com/watch?v=lxnR5iJySjM&ab_channel=RoyaltyFreeMusic-NoCopyrightMusic.
+2. Press Left-Click to jump.
+3. Playtimer
+
+## How To Install The Game
+1. Go to [Standalone Folder] and download the zip.
+2. Unzip and click EndlessRunner.exe
+3. Click Play! to start the game
